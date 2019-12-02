6963c8272b56072ad4bd4f5c1df737d7a6eacf04
Josh Boudreau <joshuaboud@hotmail.com>
2019-12-02 16:05:05 -0400

    removed unused error message

--- error.cpp
+++ error.cpp
@@ -24,7 +24,6 @@ std::string errors[NUM_ERRORS] = {
   "Error loading configuration file.",
   "Tier directory does not exist. Please check config.",
   "THRESHOLD error. Must be a positive integer. Please check config.",
-  "Error reading tier ID ([Tier <n>]). <n> must be positive integer.",
   "First tier must be specified with '[<tier name>]' before any other config options.",
   "No tiers defined in config file.",
   "Only one tier defined in config file, two or more are needed.",
