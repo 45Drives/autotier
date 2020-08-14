a014f9621c867f85b48bbd3ddf7da87dda30977d
joshuaboud <joshuaboud@hotmail.com>
2020-08-14 11:14:32 -0300

    document Config class

--- src/config.hpp
+++ src/config.hpp
@@ -34,15 +34,43 @@ enum BYTE_FORMAT {BYTES, POWTEN, POWTWO};
 class Config{
 private:
 	void generate_config(std::fstream &file);
+  /* called if config file DNE
+   * prints default config options to file
+   */
 	bool verify(const std::list<Tier> &tiers);
+  /* ensures all config options are legal
+   * returns true if no errors found, false otherwise
+   */
+  int load_global(std::fstream &config_file, std::string &id);
+  /* called by load() when [global] config header is found
+   */
 public:
 	int log_lvl = ERR;
+  /* value read from config file which may be overridden in main()
+   * by CLI flags [ --verbose | --quiet ]
+   */
   int byte_format = BYTES;
+  /* set by CLI flag in main() - sets print format for bytes as:
+   * bytes, base 1000 SI units, or base 1024 binary units
+   */
 	unsigned long period = ERR;
+  /* number of seconds between tiering
+   */
 	fs::path mountpoint;
+  /* path read from config file at which to mount autotier
+   * can be overridden with CLI flag [ -m --mountpoint </path/to/mountpoint> ]
+   */
 	void load(const fs::path &config_path, std::list<Tier> &tiers);
-	int load_global(std::fstream &config_file, std::string &id);
+  /* open config file at config_path, parse global and tier options,
+   * populate list of tiers
+   */
 	void dump(std::ostream &os, const std::list<Tier> &tiers) const;
+  /* print out loaded options from config file for the global section
+   * and for each tier to os
+   */
 };
 
 void strip_whitespace(std::string &str);
+/* modifies str by first removing comments at end if they exist,
+ * then removing all whitespace characters from the end and beginning of str.
+ */
