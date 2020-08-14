7668da9e6d41022db8b88e84e43720fc34948b62
joshuaboud <joshuaboud@hotmail.com>
2020-08-14 16:26:21 -0300

    document TierEngine class

--- src/tierEngine.hpp
+++ src/tierEngine.hpp
@@ -40,36 +40,131 @@ class Config;
 class TierEngine{
 private:
 	sqlite3 *db;
-	pid_t fusermount_pid = 0;
+  /* database holding file metadata:
+   * table Files:
+   * ID | relative path | current tier path | pin path | popularity | last atime
+   */
 	int mutex;
+  /* file handle for mutexing tiering of files
+   */
 	fs::path mutex_path;
+  /* path to mutex lock file
+   */
 	Config config;
+  /* configuration goes here, read in from file in TierEngine::TierEngine()
+   */
 	fs::path get_mutex_name(const fs::path &config_path);
+  /* returns RUN_PATH/<hash of config file path>.autotier.lock
+   */
 	int lock_mutex(void);
+  /* opens file at mutex_path such that if the file already exists, opening fails.
+   * Uses this as a mutex lock - if the file exists, the critical section is locked.
+   * To unlock, delete the file
+   */
 	void unlock_mutex(void);
+  /* deletes mutex lock file
+   */
 	void open_db(void);
+  /* creates connection with sqlite database db
+   */
   std::string print_bytes(unsigned long long bytes);
+  /* formats number of bytes as either bytes, SI units (base 1000),
+   * or binary units (base 1024). Accepts number of bytes, returns formatted 
+   * string
+   */
 protected:
 	std::list<Tier> tiers;
+  /* list of tiers build from configuration file
+   */
 	std::list<File> files;
+  /* list to contain every file across all tiers for sorting
+   */
 public:
 	TierEngine(const fs::path &config_path);
+  /* attaches signal handlers, loads config, picks run path for mutex lock and
+   * db with pick_run_path(), constructs mutex_path,
+   * opens database with open_db()
+   */
 	~TierEngine(void);
+  /* closes database
+   */
 	fs::path get_mountpoint(void);
+  /* returns mountpoint path from config
+   */
 	std::list<Tier> &get_tiers(void);
+  /* returns reference to list of tiers
+   */
   Tier *tier_lookup(fs::path p);
+  /* returns pointer to tier with path p
+   * useful for file pinning
+   */
   Config *get_config(void);
+  /* returns pointer to config for modification
+   */
 	void begin(bool daemon_mode);
+  /* IF daemon_mode THEN
+   * starts tiering daemon
+   *    WHILE running DO
+   *      launch_crawlers(), calc_popularity() once per second
+   *      sort(), simulate_tier(), move_files() once per tier period
+   *    END DO
+   * mounts filesystem
+   * ELSE
+   * tiers files once
+   * END IF
+   */
 	void launch_crawlers(void (TierEngine::*function)(fs::directory_entry &itr, Tier *tptr));
+  /* crawl() for each tier in tiers
+   */
 	void crawl(fs::path dir, Tier *tptr, void (TierEngine::*function)(fs::directory_entry &itr, Tier *tptr));
+  /* recurse into tier directory, executing function on each file.
+   * Function can only be emplace_file(), so this should be changed to always emplace files
+   */
 	void emplace_file(fs::directory_entry &file, Tier *tptr);
+  /* place file into std::list<File> files, constructing with fs::path, Tier*, and db
+   */
 	void print_file_pins(void);
+  /* iterate through list of files, printing old_path and pinnned_to to std::cout
+   */
 	void print_file_popularity(void);
+  /* iterate through list of files, printing popularity
+   */
 	void sort(void);
+  /* sorts list of files based on popularity, if pop1 == pop2, sort by atime
+   */
 	void simulate_tier(void);
+  /* find out which tier each file belongs in
+   * LET file = first file of sorted files
+   * LET tier = first tier
+   * LET tier_usage = size of pinned files
+   * WHILE file != last file of sorted files DO
+   *    IF tier_usage + size of file > tier watermark AND tier != last tier THEN
+   *        tier = next tier
+   *        tier_usage = 0
+   *    END IF
+   *    file belongs to tier (push &file into Tier::incoming_files)
+   *    tier_usage += size of file
+   *    file = next file
+   * END DO
+   */
 	void move_files(void);
+  /* start at last tier, move files into tier based on outcome of simulate_tier()
+   */
 	void print_tier_info(void);
+  /* iterate through list of tiers, printing ID, path, current usage, and watermark
+   * uses format_bytes() for printing current usage and watermark
+   */
 	void pin_files(std::string tier_name, std::vector<fs::path> &files_);
+  /* pin each file in files_ to tier referenced by tier_name
+   * TODO: use tier_lookup to find tier, remove non-existant files from db
+   * possibly just use SQL directly to accomplish this
+   */
 	void unpin(int optind, int argc, char *argv[]);
+  /* remove pinned_to path from each file passed
+   * TODO: remove non-existant files from db
+   * possibly just use SQL directly to accomplish this
+   */
 	void calc_popularity(void);
+  /* call File::calc_popularity() for each file in files
+   */
 };
