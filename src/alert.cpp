ff8e38ca09ab718745d988e565944935b6a903b4
joshuaboud <joshuaboud@hotmail.com>
2021-01-22 13:28:59 -0400

    Update logging method Use a Logger class with a global object in the Logging namespace

--- src/alert.cpp
+++ src/alert.cpp
@@ -1,56 +1,58 @@
 /*
-		Copyright (C) 2019-2020 Joshua Boudreau <jboudreau@45drives.com>
-		
-		This file is part of autotier.
-
-		autotier is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-
-		autotier is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-		GNU General Public License for more details.
-
-		You should have received a copy of the GNU General Public License
-		along with autotier.	If not, see <https://www.gnu.org/licenses/>.
-*/
+ *    Copyright (C) 2019-2021 Joshua Boudreau <jboudreau@45drives.com>
+ * 
+ *    This file is part of autotier.
+ * 
+ *    autotier is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    autotier is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with autotier.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "alert.hpp"
-#include "config.hpp"
 #include <iostream>
-#include <mutex>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <algorithm>
 
-std::mutex alert_mutex;
-
-int log_lvl = -1;
-
-std::string errors[NUM_ERRORS] = {
-	"Error loading configuration file.",
-	"Tier directory does not exist. Please check config.",
-	"First tier must be specified with '[<tier name>]' before any other config options.",
-	"No tiers defined in config file.",
-	"Only one tier defined in config file, two or more are needed.",
-	"WATERMARK must be a positive integer between 0 and 100.",
-	"Error setting extended attribute.",
-	"Error reading log level.",
-	"Error reading tier period.",
-	"Error constructing mutex file path.",
-	"Error mounting mergerfs.",
-	"Error forking.",
-	"Error opening database.",
-	"Error: mountpoint does not exist or is not a directory.",
-  "Error creating run path."
-};
-
-void error(enum Error error){
-	std::cerr << errors[error] << std::endl;
+namespace Logging{
+	Logger log(1);
 }
 
-void Log(std::string msg, int lvl){
-	if(log_lvl < lvl) return;
-	alert_mutex.lock();
-	std::cout << msg << std::endl;
-	alert_mutex.unlock();
+Logger::Logger(int log_level){
+	log_level_ = log_level;
+}
+
+void Logger::message(const std::string &msg, int lvl) const{
+	if(log_level_ >= lvl) std::cout << msg << std::endl;
+}
+
+void Logger::warning(const std::string &msg) const{
+	std::cerr << "Warning: " << msg << std::endl;
+}
+
+void Logger::error(const std::string &msg, bool exit_) const{
+	std::cerr << "Error: " << msg << std::endl;
+	if(exit_) exit(EXIT_FAILURE);
+}
+
+#define N_INDEX 9
+std::string Logger::format_bytes(uintmax_t bytes) const{
+	if(bytes == 0) return "0 B";
+	std::stringstream formatted_ss;
+	std::string units[N_INDEX] = {" B", " KiB", " MiB", " GiB", " TiB", " PiB", " EiB", " ZiB", " YiB"};
+	int i = std::min(int(log(bytes) / log(1024.0)), N_INDEX - 1);
+	double p = pow(1024.0, i);
+	double formatted = double(bytes) / p;
+	formatted_ss << std::fixed << std::setprecision(2) << formatted << units[i];
+	return formatted_ss.str();
 }
--- src/alert.hpp
+++ src/alert.hpp
@@ -1,38 +1,60 @@
 /*
-		Copyright (C) 2019-2020 Joshua Boudreau <jboudreau@45drives.com>
-		
-		This file is part of autotier.
-
-		autotier is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-
-		autotier is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-		GNU General Public License for more details.
-
-		You should have received a copy of the GNU General Public License
-		along with autotier.	If not, see <https://www.gnu.org/licenses/>.
-*/
+ *    Copyright (C) 2019-2021 Joshua Boudreau <jboudreau@45drives.com>
+ *    
+ *    This file is part of autotier.
+ * 
+ *    autotier is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    autotier is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with autotier.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
 #include <string>
 
-#define ERR -1
-
-extern int log_lvl;
-
-#define NUM_ERRORS 15
-enum Error{
-  LOAD_CONF, TIER_DNE, NO_FIRST_TIER, NO_TIERS, ONE_TIER, WATERMARK_ERR, SETX,
-  LOG_LVL, PERIOD, GET_MUTEX_NAME, MOUNT, FORK, OPEN_DB, MOUNTPOINT, CREATE_RUNPATH
+class Logger{
+private:
+	int log_level_;
+	/* Value from config file. Each log message
+	 * passes a log level to check against this number.
+	 * If the message's level is higher, it is printed.
+	 */
+public:
+	explicit Logger(int log_level);
+	/* Constructs Logger, assigning log_level to the
+	 * internal log_level_.
+	 */
+	void message(const std::string &msg, int lvl) const;
+	/* Print message to stdout if lvl >= log_level_.
+	 * Use this for regular informational log messages.
+	 */
+	void warning(const std::string &msg) const;
+	/* Print message to stderr prepended with "Warning: ".
+	 * Use this for non-fatal errors.
+	 */
+	void error(const std::string &msg, bool exit_ = true) const;
+	/* Print message to stderr prepended with "Error: ".
+	 * Exits with EXIT_FAILURE if exit_ is left as true.
+	 * exit_ is used to delay exiting until later if there are
+	 * multiple errors to print.
+	 */
+	std::string format_bytes(uintmax_t bytes) const;
+	/* Return bytes as string in base-1024 SI units.
+	 */
 };
 
-void error(enum Error error);
-
-void Log(std::string msg, int lvl);
-/* print msg to std::cout if lvl >= log_lvl from config or CLI opts
- */
+namespace Logging{
+	extern Logger log;
+	/* Global Logger object. Use Logging::log.<method> in source
+	 * files including this header.
+	 */
+}
