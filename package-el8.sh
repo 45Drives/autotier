a396f816d246688d02f20e28e65a89b43759352e
joshuaboud <joshuaboud@hotmail.com>
2021-04-08 14:49:01 -0300

    fix name in disclaimer

--- package-el8.sh
+++ package-el8.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 #    Copyright (C) 2019-2021 Joshua Boudreau <jboudreau@45drives.com>
+#    
+#    This file is part of autotier.
 # 
-#    This file is part of cephgeorep.
-# 
-#    cephgeorep is free software: you can redistribute it and/or modify
+#    autotier is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 2 of the License, or
+#    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 # 
-#    cephgeorep is distributed in the hope that it will be useful,
+#    autotier is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 # 
 #    You should have received a copy of the GNU General Public License
-#    along with cephgeorep.  If not, see <https://www.gnu.org/licenses/>.
+#    along with autotier.  If not, see <https://www.gnu.org/licenses/>.
 
 TMP_DIR=/tmp/autotier
 
