7511959e5d965dac8d83cb1b525c30d8746e3c41
joshuaboud <joshuaboud@hotmail.com>
2021-04-20 11:59:06 -0300

    fix file pinning

--- src/impl/autotierfs/tierEngine/adHoc/executeQueued.cpp
+++ src/impl/autotierfs/tierEngine/adHoc/executeQueued.cpp
@@ -20,6 +20,11 @@
 #include "tierEngine.hpp"
 #include "alert.hpp"
 
+extern "C" {
+	#include <sys/stat.h>
+	#include <sys/time.h>
+}
+
 void TierEngine::execute_queued_work(void){
 	while(!adhoc_work_.empty()){
 		AdHoc work = adhoc_work_.pop();
@@ -53,17 +58,32 @@ void TierEngine::pin_files(const std::vector<std::string> &args){
 	for(std::vector<std::string>::const_iterator fptr = std::next(args.begin()); fptr != args.end(); ++fptr){
 		fs::path mounted_path = *fptr;
 		fs::path relative_path = fs::relative(mounted_path, mount_point_);
-		Metadata f(relative_path.c_str(), db_);
+		Metadata f(relative_path.string(), db_);
 		if(f.not_found()){
 			Logging::log.warning("File to be pinned was not in database: " + mounted_path.string());
 			continue;
 		}
-		Tier *old_tptr = tier_lookup(fs::path(f.tier_path()));
-		File file(old_tptr->path() / relative_path, db_, old_tptr);
-		file.pin();
-		tptr->enqueue_file_ptr(&file);
+		fs::path old_path = f.tier_path()/relative_path;
+		fs::path new_path = tptr->path()/relative_path;
+		struct stat st;
+		if(stat(old_path.c_str(), &st) == -1){
+			Logging::log.warning("stat failed on " + old_path.string() + ": " + strerror(errno));
+			continue;
+		}
+		struct timeval times[2];
+		times[0].tv_sec = st.st_atim.tv_sec;
+		times[0].tv_usec = st.st_atim.tv_nsec / 1000;
+		times[1].tv_sec = st.st_mtim.tv_sec;
+		times[1].tv_usec = st.st_mtim.tv_nsec / 1000;
+		if(tptr->move_file(old_path, new_path, config_.copy_buff_sz())){
+			utimes(new_path.c_str(), times);
+			f.pinned(true);
+			f.tier_path(tptr->path().string());
+			f.update(relative_path.string(), db_);
+		}
 	}
-	tptr->transfer_files(config_.copy_buff_sz());
+	if(!config_.strict_period())
+		tier(std::chrono::seconds(-1));
 }
 
 void TierEngine::unpin_files(const std::vector<std::string> &args){
